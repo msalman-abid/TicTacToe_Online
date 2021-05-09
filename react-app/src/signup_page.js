0fd68539088a69c790f31b6dca658471d5b85308
nasir-azeemi <54790791+nasir-azeemi@users.noreply.github.com>
2021-05-09 22:46:49 +0500

    Update signup_page.js

--- react-app/src/signup_page.js
+++ react-app/src/signup_page.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import {useState}  from "react";
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-
+import {
+  Button,
+  Box,
+  Container,
+  CssBaseline,
+  Grid,
+  Link,
+  TextField,
+  makeStyles
+} from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -41,8 +43,19 @@ export default function SignUp() {
   const classes = useStyles();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
+    <Box
+    sx={{
+    backgroundColor: 'background.default',
+    minHeight: '100%',
+    py: 3
+    }}
+    >
+  
+    <Button>
+      Main Page
+    </Button>
+    
+    <Container maxWidth="xs">
       <div className={classes.paper}>
         <form className={classes.form} noValidate onSubmit={handleSubmit}>
           <Grid container spacing={2}>
@@ -110,5 +123,6 @@ export default function SignUp() {
         </form>
       </div>
     </Container>
+    </Box>
   );
 }
