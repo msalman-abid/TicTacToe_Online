80f6c7d06067fb2ff8fb0343bfca345b59e5b451
Omema Ahmed <51672463+OmemaA@users.noreply.github.com>
2021-05-11 18:25:20 +0500

    Signup/Login Implementation (buggy)

--- express-api/routes/login.js
+++ express-api/routes/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const mysql = require('mysql');
+var qs = require('querystring');
 
 var router = express.Router();
 
@@ -7,22 +8,93 @@ function insertUser(pool, data) {
 
     let insertQuery = 'INSERT INTO ?? (??,??,??,??,??) VALUES (?,?,?,?,?)';
     let query = mysql.format(insertQuery, ["users", "email", "password", "won", "lost",
-        "draw", data.username, data.password, 0, 0, 0]);
+        "draw", data.username, data.password,0, 0, 0]);
 
-    pool.query(query, (err, response) => {
+    pool.query(query, (err, rows) => {
         if (err) {
             console.error(err);
             return;
         }
         // rows added
-        console.log(response.insertId);
+        console.log(rows.insertId);
     })
 }
+
+function getUser(pool, data) {
+
+    let insertQuery = 'SELECT * FROM ?? WHERE ?? = ?? AND ?? = ??';
+    let query = mysql.format(insertQuery, ["users", "email", data.username, "password", data.password]);
+
+    pool.query(query, (err, rows) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        // Login Sucessful
+        console.log("Rows");
+        console.log(rows);
+        // console.log("Query");
+        // console.log(query)
+        res.send(rows);
+        // res.json(rows);
+        // return rows;
+    })
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
     console.log(req.body);
     res.send("Hellow")
 });
+
+
+router.post('/signup', function(req, res, next) {
+    var pool = req.app.get("pool");
+
+    console.log(req.body);
+    data = req.body.credentials;
+    
+    setTimeout(() => {
+        insertUser(pool, data);
+    },500);
+    
+    res.send({
+        token:{
+            username: data.username,
+            won: 0,
+            lost: 0,
+            draw: 0
+        }
+    })
+    
+    
+});
+
+router.post('/login', function(req, res, next) {
+    console.log("Hello");
+    var pool = req.app.get("pool");
+    
+    // console.log(req.body);
+    data = req.body.credentials;
+    
+    setTimeout(() => {
+        getUser(pool, data);
+    },500);
+    
+    console.log("Result");
+    console.log(res);
+    res.send({
+        token:{
+            username: data.username
+            // won: ,
+            // lost: ,
+            // draw: 
+        }
+    })
+    
+});
+
 
 router.post('/', function(req, res, next) {
 
@@ -44,24 +116,5 @@ router.post('/', function(req, res, next) {
         })
     }
     
-});
-
-router.post('/signup', function(req, res, next) {
-    var pool = req.app.get("pool");
-    
-    // pool.getConnection((err, connection) => {
-    //     if (err) {
-    //         throw err;
-    //     }
-
-    // })
-    console.log(req.body);
-    data = req.body.credentials;
-    
-    setTimeout(() => {
-        insertUser(pool, data);
-    },500);
-    
-
 });
 module.exports = router;
