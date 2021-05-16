5d804ca3915c67f76e1aa69b2082d1d3cdf2c24c
Muhammad Salman Abid <49055716+msalman-abid@users.noreply.github.com>
2021-05-17 03:53:26 +0500

    Leaderboard Prototype Added

--- express-api/app.js
+++ express-api/app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
-var mysql = require('mysql')
+var mysql = require('mysql2')
 const socketIo = require('socket.io');
 
 
--- express-api/routes/users.js
+++ express-api/routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const mysql = require('mysql2');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+
+ function getLeaderboard(pool, query) {
+  return new Promise((resolve, reject) => {
+  pool.query(query, (err, rows) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+  })
+})
+}
+
+router.get('/leaderboard',  function (req, res) {
+  var pool = req.app.get("pool");
+  
+  let selectQuery = 'SELECT * FROM ?? ORDER BY won DESC, draw DESC, lost ASC;';
+  let query = mysql.format(selectQuery, ["users"]);
+  // const q_result = await getLeaderboard(pool, query);
+
+  pool.query(query, (err, rows) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(rows);
+    res.send(rows);
+
+  })
 });
 
 module.exports = router;
